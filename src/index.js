8689db7d23af065e75f523eaddbefa1bff1b368a
Coline Soleau <coline.soleau@epfedu.fr>
2026-03-18 12:32:00 +0100

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import niveau1 from "/src/js/niveau1.js";
 import niveau2 from "/src/js/niveau2.js";
 import niveau3 from "/src/js/niveau3.js";
 import accueil from "/src/js/accueil.js";
-
+import menu from "/src/js/menu.js";
 
 var config = {
   type: Phaser.AUTO,
